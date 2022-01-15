8efa273dab3d58b325faca1126ef5088318dc4b6
Ujwal Vinay <90175613+ujwalvinay@users.noreply.github.com>
2022-01-15 19:17:40 +0530

    Main function and evaluation

---
+++ src/mainsection.java
@@ -0,0 +1,20 @@
+package numbergame;
+public class mainsection 
+{
+	void userin(String n)
+	{
+		int b=Integer.parseInt(n);
+	}
+	void compgen(int comp)
+	{
+		if(comp==b)
+			System.out.println("You won");
+		else
+			System.out.println("Better luck nexzt time");
+	}
+	public static void main(String[] args)
+	{
+		gui window = new gui();
+	}
+
+}
