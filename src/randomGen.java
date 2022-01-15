86069467e9fda6dec0cbf31b0ab8a0cd4a523d0e
Ujwal Vinay <90175613+ujwalvinay@users.noreply.github.com>
2022-01-15 19:16:46 +0530

    updated

--- src/randomGen.java
+++ src/randomGen.java
@@ -2,14 +2,13 @@ package numbergame;
 import java.util.Random;     
 public class randomGen 
 {
-	public static void main( String args[] ) {
-		
-		  gui frame = new gui();
+	void numbergen()
+	{
+	
 	      Random rand = new Random(); //instance of random class
 	      int upperbound = 25;
 	        //generate random values from 0-24
 	      int int_random = rand.nextInt(upperbound);
 	      System.out.println(int_random);
 	}
-
 }
