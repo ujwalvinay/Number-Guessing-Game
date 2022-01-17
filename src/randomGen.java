502c614c4e618fd964f1f4f1e9c947b1c9e898a9
Ujwal Vinay <90175613+ujwalvinay@users.noreply.github.com>
2022-01-17 15:21:35 +0530

    Update randomGen.java

--- src/randomGen.java
+++ src/randomGen.java
@@ -1,14 +1,30 @@
 package numbergame;
+import javax.swing.*;  
+
 import java.util.Random;     
 public class randomGen 
 {
+	JFrame j=new JFrame();
+	int int_random , b;
 	void numbergen()
 	{
 	
 	      Random rand = new Random(); //instance of random class
-	      int upperbound = 25;
-	        //generate random values from 0-24
-	      int int_random = rand.nextInt(upperbound);
+	      int upperbound = 26;
+	        //generate random values from 0-25
+	      int_random = rand.nextInt(upperbound);
 	      System.out.println(int_random);
+	}
+	void userin(String n)
+	{
+		b=Integer.parseInt(n);
+		compare();
+	}
+	void compare()
+	{
+		if(int_random==b)
+			JOptionPane.showMessageDialog(j,"You won");
+		else
+			JOptionPane.showMessageDialog(j,"Computer's choice :"+int_random+"\nBetter luck next time");
 	}
 }
